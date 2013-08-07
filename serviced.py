80524772353b55d6f627b43d271a00f64a282df9
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-08-07 16:20:28 +0000

    Development on serviced.py

--- serviced.py
+++ serviced.py
@@ -2,6 +2,29 @@
 
 import os
 import sys
+import time
 import daemon
+import signal
+import ConfigParser
 
-print "Hello!"
+CONFIG_FILE = "/usr/csee/etc/serviced.conf"
+
+def getConfig(configfile):
+
+def reloadConfig(config):
+  config = getConfig(configFile)
+
+def checkState(config):
+
+def main():
+  config = getConfig(CONFIG_FILE)
+  context = daemon.DaemonContext()
+  context.signal_map = {
+    signal.SIGUSR1: reloadConfig(config),
+    }
+  with context:
+    while True:
+      checkState(config)
+      time.sleep(config['sleepTime'])
+    
+main()
