c44136018d095a4d30ba2e00f80771036a3c9110
mark25@UMBC.EDU <mark25@UMBC.EDU@bb2173db-233b-63cc-8cb0-ec26fdc3f785>
2013-08-07 17:53:09 +0000

    More work

--- serviced.py
+++ serviced.py
@@ -5,26 +5,51 @@ import sys
 import time
 import daemon
 import signal
+import lockfile
 import ConfigParser
 
 CONFIG_FILE = "/usr/csee/etc/serviced.conf"
+LOCK_FILE = "/var/lock/serviced.pid"
 
 def getConfig(configfile):
+  config = ConfigParser.ConfigParser()
+  config.readfp(open(configfile))
+  return dict((key, value) for (key, value) in config.items("main"))
+
+config = getConfig(CONFIG_FILE)
 
 def reloadConfig(config):
-  config = getConfig(configFile)
+  config = getConfig(CONFIG_FILE)
 
 def checkState(config):
+  processes = []
+  processes.extend([process(x, "start") for x in config['start'].split(" ")])
+  processes.extend([process(x, "stop") for x in config['stop'].split(" ")])
+  
+  
 
 def main():
-  config = getConfig(CONFIG_FILE)
   context = daemon.DaemonContext()
   context.signal_map = {
     signal.SIGUSR1: reloadConfig(config),
     }
+  context.pidfile = lockfile.FileLock(LOCK_FILE)
   with context:
     while True:
       checkState(config)
       time.sleep(config['sleepTime'])
     
 main()
+
+class process:
+  def __init__(self, name, state):
+    self.name = name
+    self.status = "Unknown"
+    self.desiredState = state
+    methods = {
+      "mrepo": (serviceStatus, serviceSet),
+      }
+    if self.name in methods.keys():
+      self.checkMethod, self.setMethod = methods[self.name]
+    else:
+      self.checkMethod, self.setMethod = (serviceStatus, serviceSet)
